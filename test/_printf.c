f4e6ba41533d25211eaddb3a485c8f888e067a03
rukshar06 <saibrukshar@gmail.com>
2026-06-04 15:02:59 +0400

    printf

--- test/_printf.c
+++ test/_printf.c
@@ -2,8 +2,64 @@
 /**
  *_printf - produces output according to a format
  *@format: specifier
+ *Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
+	va_list args;
+	int i = 0, count = 0;
+	char *str;
+
 	if (format == NULL)
 		return (-1);
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+
+			switch (format[i])
+			{
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+
+				case 's':
+					str = va_arg(args, char*);
+					if (str == NULL)
+						str = "(null)";
+
+					while (*str)
+					{
+						_putchar(*str);
+						str++;
+						count++;
+					}
+					break;
+
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
--- test/main.h
+++ test/main.h
@@ -7,5 +7,9 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_integer(va_list args);
 
 #endif
